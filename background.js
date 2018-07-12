b47f006c0ec19151158b427dbb981cd9726737b9
Alan Wang <zhigang.wang@gmail.com>
2018-07-12 15:58:26 -0400

    Close caption is working. Need more cosmetic works.

--- background.js
+++ background.js
@@ -31,16 +31,55 @@ function convertChinesePunctuation(text) {
     return res;
 }
 
+function openCC() {
+    var injection = " \
+    var div = document.getElementById('JustTTS_CC'); \
+    if (div === null) { \
+	var div = document.createElement('DIV'); \
+	div.setAttribute('id', 'JustTTS_CC'); \
+	div.setAttribute('style','background-color:#DDDDDD;width:400px;height:50px;position:absolute;left:0;right:0;top:90%; bottom:10%;margin:auto;max-width:100%;max-height:100%;overflow:auto;cursor: move;z-index:1000'); \
+	document.body.appendChild(div); \
+} ; ";
+
+    chrome.tabs.executeScript(null, {
+	code: injection
+    });
+    
+}
+
+function closeCC() {
+    var injection = " \
+    var cc = document.getElementById('JustTTS_CC'); \
+    if (cc !== null) \
+        cc.parentNode.removeChild(cc);";
+
+    chrome.tabs.executeScript(null, {
+	code: injection
+    });
+}
+
+function setCC(text) {
+    var injection = " \
+    var cc = document.getElementById('JustTTS_CC'); \
+    if (cc !== null) \
+        cc.innerHTML = '" + text + "'; ";
+    
+    chrome.tabs.executeScript(null, {
+	code: injection
+    });
+}
+
 async function speak(textToSpeak, language, rate) {
     var convertedText = convertChinesePunctuation(textToSpeak);
-    sentences = convertedText.split(/[.!?:]+/);
+    sentences = convertedText.split(/[.,!?:]+/);
     
     var u = new SpeechSynthesisUtterance();
     u.lang = language; 
     u.rate = rate;
+    openCC();
     for (var i=0;i< sentences.length;i++) {
-	u.text = sentences[i]; 
-	//console.log(u.text);
+	u.text = sentences[i];
+	setCC(u.text);
 	speechSynthesis.speak(u);
 
 	// Wait until finish
@@ -51,8 +90,8 @@ async function speak(textToSpeak, language, rate) {
 		break;
 	    }
 	}
-	
     }
+    closeCC();
 
 }
 
